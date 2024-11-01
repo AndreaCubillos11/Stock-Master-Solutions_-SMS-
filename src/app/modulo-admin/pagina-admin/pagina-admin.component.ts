e662152cf1724d4b98b613994e1d7d9c5865ead4
SergioPC <sergiolb@hotmail.es>
2024-10-31 23:19:56 -0500

    9no Commit
    Se agrego los botones para el CRUD de tiendas en la pagina de admin general

--- src/app/modulo-admin/pagina-admin/pagina-admin.component.ts
+++ src/app/modulo-admin/pagina-admin/pagina-admin.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuariosService } from '../serviciosAdministradores/usuarios.service';
 import { CookieService } from 'ngx-cookie-service';
-import { FormsModule } from '@angular/forms';
 import { TiendasService } from '../serviciosAdministradores/tiendas.service';
 
 @Component({
@@ -17,13 +16,19 @@ export class PaginaAdminComponent {
     private tiendasService: TiendasService
   ) { }
 
-
+  mostrarCRUD: boolean = false;
   selectedOption: string = 'optionTienda'; // Valor inicial
 
   datosBtn = [
     { texto: 'Agregar entidad producto', img: 'Añadir.svg', nombreClase: 'agregar', accion: this.agregarProducto.bind(this) },
     { texto: 'Modificar producto', img: 'ModificarProducto.svg', nombreClase: 'modificar', accion: this.modificarProducto.bind(this) },
     { texto: 'Eliminar producto', img: 'Eliminar.svg', nombreClase: 'eliminar', accion: this.eliminarProducto.bind(this) }
+  ];
+
+  datosBtnTiendas = [
+    { texto: 'Agregar nueva tienda', img: 'Añadir.svg', nombreClase: 'agregar', accion: this.agregarTienda.bind(this) },
+    { texto: 'Modificar datos de tienda', img: 'ModificarProducto.svg', nombreClase: 'modificar', accion: this.modificarTienda.bind(this) },
+    { texto: 'Eliminar tienda', img: 'Eliminar.svg', nombreClase: 'eliminar', accion: this.eliminarTienda.bind(this) }
   ];
 
   agregarProducto() {
@@ -39,5 +44,22 @@ export class PaginaAdminComponent {
   eliminarProducto() {
     console.log('Eliminar tienda');
     this.router.navigate(['/eliminarProducto']);
+  }
+
+  agregarTienda() {
+    //Hacer la logica
+  }
+
+  modificarTienda() {
+    //Hacer la logica
+  }
+
+  eliminarTienda() {
+    //Hacer la logica
+  }
+
+  toggleCRUD() {
+    console.log('presionado');
+    this.mostrarCRUD = !this.mostrarCRUD;
   }
 }
